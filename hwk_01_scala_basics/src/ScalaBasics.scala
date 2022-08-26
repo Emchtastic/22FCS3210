d021a20f302a486900863af98d65fbe0d1e10457
Alex Emch <aemch@msudenver.edu>
2022-08-26 10:53:02 -0600

    Update ScalaBasics.scala

--- hwk_01_scala_basics/src/ScalaBasics.scala
+++ hwk_01_scala_basics/src/ScalaBasics.scala
@@ -10,12 +10,20 @@ object ScalaBasics {
   def main(args: Array[String]): Unit = {
 
     // TODO #1: declare an integer constant named MAX and set it to 10; declare an integer variable named sum and initialized it to 0
-
+    val MAX: Int = 10
+    var sum: Int = 0
 
     // TODO #2: write a for loop to compute sum as 1 + 2 + ... + MAX; then, display "sum is ..." substituting with the value of sum (must use variable substitution)
-
+    for(i <- 1 to MAX) {
+      sum = sum + i
+      println("sum is " + sum)
+    }
 
     // TODO #3: write a conditional to display "sum is even!" or "sum is odd!" depending on the value of sum;
 
+    if (sum % 2 == 0)
+      println("sum is even!")
+    else
+      println("sum is odd!")
   }
 }
