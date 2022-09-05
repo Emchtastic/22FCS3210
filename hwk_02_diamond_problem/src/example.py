d224d6c9e89b8a77496dd3dc54dd976a49620833
Alex Emch <aemch@msudenver.edu>
2022-09-05 11:57:52 -0600

    hw2

--- hwk_02_diamond_problem/src/example.py
+++ hwk_02_diamond_problem/src/example.py
@@ -8,34 +8,43 @@
 class Device: 
 
     def __init__(self, manufacturer) -> None:
+        self.manufacturer = manufacturer
         pass
 
     def __str__(self) -> str:
-        return ""
+        return self.manufacturer
 
 class Printer(Device): 
 
     def __init__(self, manufacturer, type) -> None:
+        self.manufacturer = manufacturer
+        self.type = type
         pass
 
     def __str__(self) -> str:
-        return ""
+        return self.manufacturer+" "+self.type
 
-class Scanner(Device): 
+class Scanner(Device):
 
     def __init__(self, manufacturer, dpi) -> None:
-        pass 
+        self.manufacturer = manufacturer
+        self.dpi = dpi
+        pass
 
     def __str__(self) -> str:
-        return ""
+        return self.manufacturer+" "+" "+self.dpi
+
 
 class MultifunctionPrinter(Printer, Scanner): 
 
-    def __init__(self, manufacturer, type, dpi) -> None:
+    def __init__(self, manufacturer, type, dpi):
+        self.manufacturer = manufacturer
+        self.type = type
+        self.dpi = dpi
         pass
 
     def __str__(self) -> str:
-        return ""
+        return self.manufacturer+" "+self.type+" "+self.dpi
 
 if __name__ == '__main__':
     ts6300 = MultifunctionPrinter("Canon", "Inkjet", "600")
