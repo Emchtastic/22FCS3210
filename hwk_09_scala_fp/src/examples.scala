59b930bb8f95162b3f5ec7289a6b4ad3177cc513
Alex Emch <aemch@msudenver.edu>
2022-11-07 13:25:06 -0700

    All good besides getChoices

--- hwk_09_scala_fp/src/examples.scala
+++ hwk_09_scala_fp/src/examples.scala
@@ -1,7 +1,7 @@
 /*
  * CS3210 - Principles of Programming Languages - Fall 2022
  * Instructor: Thyago Mota
- * Student: 
+ * Student: Alex Emch
  * Description: Homework 09 - Functional Programming Examples
  */
 
@@ -32,13 +32,45 @@ object Examples {
     (1 until m).filter((r: Int) => coprime(m)(r))
 
   // TODO #1: e) write function primeFactors that takes an integer and returns a flat list with the prime factors of the given number in ascending order.
-  def primeFactors(x: Int) = { }
+  def primeFactors(x: Int): List[Int] = {
+    var list = List.empty[Int]
+    for {
+      num <- 2.to(x)
+      if x % num == 0 && isPrime(num)
+    } list :+= num
+    list
+  }
 
   // TODO #2: f) write function primeFactorsMult similar to primeFactors but with the prime factors and their multiplicity.
-  def primeRange(x: Int) = { }
+  // Had a bit of a difficult time casting the List as a sequence of primes and their multiplicity. Made a string instead an concatenated to it.
+  def primeFactorsMult(x: Int) = {
+    var list = ""
+    var a = 0
+    var b = 0
+    for (num <- 2.to(x)){
+      if (x % num == 0 && isPrime(num)) {
+        a = num
+        for (num2 <- 1.to(x)){
+          var y = x%scala.math.pow(a,num2)
+          if (x%scala.math.pow(a,num2)==0) {
+            b = num2
+          }
+        }
+        list += (a, b) + " "
+      }
+    }
+    list
+  }
 
   // TODO #3: g) write function primesRange that takes a range of integers and returns a list of all prime numbers within that range.
-  def primesRange(a: Int, b: Int) = { }
+  def primesRange(a: Int, b: Int) = {
+    var list = List.empty[Int]
+    for {
+      num <- a.to(b)
+      if isPrime(num)
+    } list :+= num
+    list
+  }
 
   // OPTIONAL TODO #1: h) Goldbach's conjecture says that every positive even number greater than 2 is the sum of two prime numbers. Example: 28 = 5 + 23. It is one of the most famous facts in number theory that has not been proved to be correct in the general case. It has been numerically confirmed up to very large numbers. Write function goldbach that takes an integer and returns the two prime numbers that sum up to it.
   def goldbach(x: Int) = { }
@@ -47,6 +79,8 @@ object Examples {
   def goldbachList(a: Int, b: Int) = { }
 
   def main(args: Array[String]): Unit = {
-
+    println(primeFactors(12).mkString(" "))
+    println(primeFactorsMult(12))
+    println(primesRange(2,15).mkString(" "))
   }
 }
--- prg_02_sudoku/src/Sudoku.scala
+++ prg_02_sudoku/src/Sudoku.scala
@@ -1,63 +1,159 @@
 /*
  * CS3210 - Principles of Programming Languages - Fall 2022
  * Instructor: Thyago Mota
+ * Student: Alex Emch
  * Description: Prg 02 - Sudoku Puzzle
  */
 
+import scala.annotation.tailrec
 import scala.io._
 
 object Sudoku {
 
   // TODO #1: return an 2D array of Int representing a sudoku board given a filename
   def readBoard(fileName: String): Array[Array[Int]] = {
-    null
+    val in = Source.fromFile(fileName)
+    val lines = in.getLines().filterNot(_.isEmpty)
+    val board = lines.map(line => line.split("").map(_.toInt)).toArray
+    in.close()
+    board
   }
 
   // TODO #2: return a String representation from a given sudoku board
-  def boardToString(board: Array[Array[Int]]): String = ""
+  def boardToString(board: Array[Array[Int]]): String = {
+    board.map(_.mkString).mkString("\n")
+  }
 
   // TODO #3: return a specific row from a sudoku board as a sequence of numbers
-  def getRow(board: Array[Array[Int]], row: Int): Array[Int] = null
+  def getRow(board: Array[Array[Int]], row: Int): Array[Int] = {
+    board(row-1)
+  }
 
   // TODO #4: return a specific column from a sudoku board as a sequence of numbers
-  def getCol(board: Array[Array[Int]], col: Int): Array[Int] = null
+  def getCol(board: Array[Array[Int]], col: Int): Array[Int] = {
+    //Build new array using elements from column _
+    board.map(_(col-1))
+  }
 
   // TODO #5: return a specific box from a sudoku board as a sequence of numbers
-  def getBox(board: Array[Array[Int]], x: Int, y: Int): Array[Int] = null
+  def getBox(board: Array[Array[Int]], x: Int, y: Int): Array[Int] = {
+    val rowStart = x
+    val colStart = y
+
+    val box = for {
+      rowInt <- (rowStart*3) until (rowStart*3+3)
+      colInt <- (colStart*3) until (colStart*3+3)
+    } yield (board(rowInt)(colInt))
+    box.toArray
+  }
 
   // TODO #6: a sequence is valid if it has 9 numbers in [0-9] with possibly repeated zeros
-  def isValid(seq: Array[Int]): Boolean = false
+  def isValid(seq: Array[Int]): Boolean = {
+    //array of any duplicate 1-9 numbers in array
+    val duplicates = seq.diff(seq.distinct).distinct.filterNot(_==0)
+    //Check if elements in sequence are 0-9, length is 9, and no duplicates (0's are ok for now)
+    val check = seq.forall(Set(0,1,2,3,4,5,6,7,8,9).contains) && seq.length==9 && duplicates.isEmpty
+    check
+  }
 
   // TODO #7: return whether all rows of the given board are valid sequences
-  def allRowsValid(board: Array[Array[Int]]): Boolean = false
+  def allRowsValid(board: Array[Array[Int]]): Boolean = {
+    for {
+      el <- 1.to(9)
+      if !isValid(getRow(board, el))
+    } return false
+    true
+  }
 
   // TODO #8: return whether all columns of the given board are valid sequences
-  def allColsValid(board: Array[Array[Int]]): Boolean = false
+  def allColsValid(board: Array[Array[Int]]): Boolean = {
+    for {
+      el <- 1.to(9)
+      if !isValid(getCol(board, el))
+    } return false
+    true
+  }
 
   // TODO #9: return whether all boxes of the given board are valid sequences
-  def allBoxesValid(board: Array[Array[Int]]): Boolean = false
+  def allBoxesValid(board: Array[Array[Int]]): Boolean = {
+    for {
+      i <- 0.to(2)
+      j <- 0.to(2)
+      if !isValid(getBox(board, i, j))
+    } return false
+    true
+  }
 
   // TODO #10: a board is valid if all of its rows, columns, and boxes are also valid
-  def isValid(board: Array[Array[Int]]): Boolean = false
+  def isValid(board: Array[Array[Int]]): Boolean = {
+    allBoxesValid(board) && allColsValid(board) && allRowsValid(board)
+  }
 
-  // TODO #11: a board is complete it there is no zero
-  def isComplete(board: Array[Array[Int]]): Boolean = false
+  // TODO #11: a board is complete if there is no zero
+  def isComplete(board: Array[Array[Int]]): Boolean = {
+    for{
+      el <- 1.to(9)
+      if getRow(board, el).contains(0)
+    } return false
+    true
+  }
 
   // TODO #12: a board is solved if is complete and valid
-  def isSolved(board: Array[Array[Int]]): Boolean = false
+  def isSolved(board: Array[Array[Int]]): Boolean = {
+    isValid(board) && isComplete(board)
+  }
 
   // TODO #13: return a new board configuration from the given one by setting a digit at a specific (row, col) location
-  def getChoice(board: Array[Array[Int]], row: Int, col: Int, d: Int): Array[Array[Int]] = null
+  def getChoice(board: Array[Array[Int]], row: Int, col: Int, d: Int): Array[Array[Int]] = {
+    val newBoard = board.clone()
+    newBoard(row)(col) = d
+    if (isValid(newBoard)) {
+      println(boardToString(newBoard)+"\n\n")
+      return newBoard
+    } else
+      newBoard(row)(col) = 0
+      getChoice(newBoard, row, col, d+1)
+    /**
+    val newBoard = board
+    if (row >= 9) return newBoard
+    else if (col >= 9) getChoice(board, row+1, col, 0)
+    else if (newBoard(row)(col)>0) getChoice(board, row, col+1, 0)
+    else
+      newBoard(row)(col)=d+1
+      if (isValid(newBoard)) {
+        return newBoard
+      } else
+      newBoard(row)(col)=0
+      getChoice(newBoard,row,col,d+1)
+    **/
+  }
 
   // TODO #14: return all possible new board configurations from the given one
-  def getChoices(board: Array[Array[Int]]): IndexedSeq[Array[Array[Int]]] = null
+  def getChoices(board: Array[Array[Int]]): IndexedSeq[Array[Array[Int]]] = {
+    val newBoard = board.clone()
+
+    for {
+      row <- 0.to(8)
+      col <- 0.to(8)
+      d <- 1.to(9)
+      if (newBoard(row)(col) == 0)
+    } yield getChoice(newBoard,row,col,1)//, println(boardToString(getChoice(newBoard,row,col,1)) + "\n"))
+
+  }
 
   // TODO #15: return a solution to the puzzle (null if there is no solution)
-  def solve(board: Array[Array[Int]]): Array[Array[Int]] = null
+  def solve(board: Array[Array[Int]]): Array[Array[Int]] = {
+    var t = getChoices(board)
+    if (t.isEmpty) return null
+    if (isSolved(t(0)))
+    t(0)
+    else solve(t(1))
+  }
 
   def main(args: Array[String]): Unit = {
-    val board = readBoard("sudoku1.txt")
+    val board = readBoard("sudoku3.txt")
     val sol = solve(board)
+    println(isSolved(sol))
     println(boardToString(sol))
   }
 }
