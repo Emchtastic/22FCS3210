1569538c70b2c97ff646d25491802a556ce7000f
Alex Emch <aemch@msudenver.edu>
2022-11-16 17:11:47 -0700

    11-16 hw11

--- hwk_11_prolog_lists/src/hwk11.pl
+++ hwk_11_prolog_lists/src/hwk11.pl
@@ -1,5 +1,16 @@
 % CS3210 - Principles of Programming Languages - Fall 2022
 % Instructor: Thyago Mota
 % Description: Homework 11 - Prolog Lists
-% Student Name:
+% Student Name: Alex Emch
+
+sum([], 0).
+sum([H|T], Sum) :-
+   sum(T, X), % returning base case as sum of sums
+   Sum is H + X. % add base case to head
+
+max([X],X).
+max([H|T], Y) :-
+    max(T,X),
+    (H > X -> H = Y; Y = X).
+
 
