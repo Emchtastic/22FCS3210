8d47c4b1b8435655cc649980296520ed2a957cc5
Alex Emch <aemch@msudenver.edu>
2022-11-28 12:05:11 -0700

    11-28

--- prg_03_es/src/es.pl
+++ prg_03_es/src/es.pl
@@ -1,4 +1,23 @@
 % CS3210 - Principles of Programming Languages - Fall 2022
 % Instructor: Thyago Mota
-% Description: 
-% Student(s) Name(s):
+% Description: A cheese preference finder
+% Student(s) Name(s): Alex Emch
+
+
+:- dynamic fact/1.
+
+is_true(Question) :-
+  (
+    fact(Question) ->
+      true;
+      format('~w?~n', [Question]),
+      Answer = read(yes),
+      ( Answer -> assert(fact(Question)) )
+  ).
+
+cheese(parmesan)  :- is_true('Do you like strong flavor'),      is_true('Do you like hard cheese').
+cheese(blue_cheese)  :- is_true('Do you like strong flavor'),      is_true('Do you like soft cheese').
+cheese(gouda) :- is_true('Do you like soft cheese').
+
+
+begin :- ( cheese(A) -> format('I think your cheese is a ~w.~n', [A]); false ).
